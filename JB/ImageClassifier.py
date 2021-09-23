3eca2c73ec708c86bbcaf6f70b96ce26eb6b0719
seub <brice.loustau@gmail.com>
2021-09-23 14:42:28 +0200

    started working on JB

--- JB/ImageClassifier.py
+++ JB/ImageClassifier.py
@@ -1,3 +1,32 @@
 from PIL import Image
 import numpy as np
 import pickle
+
+
+class ImageClassifier:
+    def __init__(self, database):
+        pass
+        
+    def __str__(self):
+        pass
+        
+    @staticmethod
+    def createDatabase(imageFolder, databasename):
+        pass
+        
+    def openDatabase(self):
+        pass
+        
+    @staticmethod
+    def normalizeBinary(image):
+        pass
+        
+    @staticmethod
+    def normalizeNot(image):
+        pass
+        
+    def normalizeDatabase(self, normFunction):
+        pass
+        
+    def classifyImage(self, img, normFunction):
+        pass
